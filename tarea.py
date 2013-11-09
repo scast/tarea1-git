726ee917b88edde954409187e3aeeb4b31d2752e
Simón Castillo <simoncastillo@gmail.com>
2013-11-09 00:40:15 -0430

    how to run the code!

--- tarea.py
+++ tarea.py
@@ -1,6 +1,7 @@
 import string
 import sys
 
+# wget -qO- http://norvig.com/ngrams/sowpods.txt | python tarea.py
 rep = set(a for x in sys.stdin for (a, b) in zip(x[:-1], x[1:]) if a == b)
 u = set(string.ascii_uppercase)
 print u-rep
